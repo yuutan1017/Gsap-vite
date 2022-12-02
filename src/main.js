f0dd08e3f1026ca81b39a974eea389e2c2d1b7ee
yuutan1017 <n.you0611@gmail.com>
2022-12-02 15:24:13 +0900

    images

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 import gsap from 'gsap';
 
+// setTimeout(() => {
+//   gsap.fromTo(
+//     '.box',
+//     {
+//       opacity: 0,
+//       duration: 1,
+//       y: '-200px',
+//     },
+//     {
+//       opacity: 1,
+//       duration: 2,
+//       rotation: 360,
+//       y: '100px',
+//     }
+//   );
+// }, 2000);
+
 gsap.to('.box', {
-  duration: 1,
-  x: '200px',
-  y: '100px',
-});
+  backgroundColor: 'blue',
+  position: 'relaitve',
+
+  duration: 3,
+  y: '5rem',
+
+  scaleY: 1.3,
+  scaleY: 1.3,
+  rotation: 180
+})
